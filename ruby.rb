48499c145d2368a567343d0b5e4dbd1a5eb34ca3
onifernando1 <onifernando1@gmail.com>
2022-08-06 23:26:23 +0100

    Pseudocode

---
+++ ruby.rb
@@ -0,0 +1,13 @@
+# Pseudocode for connect 4 
+
+# 1. Board - 7x6 - empty circles 
+# 2. Input player names - player a and b 
+# 3. Ask player 1 to go, select a collumn 
+# 4. Place counter all the way to bottom of collum 
+# 5. Ask player 2 to go, select a collumn, place counter to bottom 
+# 6. Check_win? if four in a row vertically/horizontally/diagonally player x wins 
+# 7. End Loop 
+
+class Board 
+
+end 
