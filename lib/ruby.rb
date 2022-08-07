cadf4b9008215b2b0c696f96d0f867539fda2b5f
onifernando1 <onifernando1@gmail.com>
2022-08-07 16:21:50 +0100

    Get move function, make move function

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -11,7 +11,7 @@ require 'colorize'
 # 7. End Loop 
 
 class Board 
-    attr_accessor :board
+    attr_accessor :board, :circle
 
     def initialize 
 
@@ -67,34 +67,76 @@ class Player
 end 
 
 class PlayerOne < Player
-    attr_reader :p1_name
+    attr_reader :name, :symbol
 
     def initialize(p1_name)
-        @p1_name = p1_name
-        @p1_symbol = "\u2B24".encode("utf-8").red
+        @name = p1_name
+        @symbol = " \u2B24".encode("utf-8").red
 
     end 
 
 end 
 
 class PlayerTwo < Player
-    attr_reader :p2_name
+    attr_reader :name, :symbol
 
     def initialize(p2_name)
-        @p2_name = p2_name
-        @p2_symbol = "\u2B24".encode("utf-8").yellow
+        @name = p2_name
+        @symbol = " \u2B24".encode("utf-8").yellow
 
     end 
 
 end 
 
-board = Board.new()
-board.make_board()
-# board.show_board()
-# board.change_board(1,3,"X")
-board.show_board()
-player = Player.new()
-player.get_p1_name
-player.get_p2_name
-player.create_players
-p player.p1.p1_name
+class Game
+    
+    attr_accessor :new_board
+    
+    def initialize 
+        @new_board = Board.new()
+        @new_board.make_board()
+        @board = @new_board.board
+        @player = Player.new()
+        @player.get_p1_name
+        @player.get_p2_name
+        @player.create_players
+        @p1 = @player.p1
+        @p2 = @player.p2
+        @current_player = @p1        
+    end 
+
+    def get_move
+        puts "#{@current_player.name}, select a collumn"
+        @player_move = gets.chomp.to_i
+        @player_move -=1
+        puts @player_move
+    end 
+
+    def make_move
+       
+        @bottom = 5 
+
+        until @board[@player_move][@bottom] === @new_board.circle || @bottom < 0
+            puts "BOTTOM IS #{@bottom}"
+            @bottom -= 1 
+        end 
+        
+        if @bottom < 0 
+            puts "INVALID"
+        else 
+            puts "BOTTOM IS #{@bottom}"
+            @board[@bottom][@player_move] = @current_player.symbol
+        end 
+
+
+    end 
+
+end 
+
+game = Game.new()
+game.get_move()
+game.make_move()
+game.new_board.show_board()
+game.get_move()
+game.make_move()
+game.new_board.show_board()
